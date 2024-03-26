796fde692aabe2846cd080fc640a52bc611ffe46
seanbet45 <164899707+seanbet45@users.noreply.github.com>
2024-03-26 15:33:45 +0000

    Update Bet45.sol

--- contracts/Bet45.sol
+++ contracts/Bet45.sol
@@ -1,17 +1,16 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
-//** Bet45 Token */
-pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+//** Bet45 Token */
+pragma solidity ^0.8.19;
+
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-/// @title Official Bet45 Token
-/// @author Bet45 team - info@bet45.co
+/// @title Bet45 Token
+/// @author bet45.co
 /// @dev A token based on OpenZeppelin's principles
-/// @custom:security-contact info@bet45.co
 
-contract Bet45 is ERC20, ERC20Burnable {
+contract Bet45 is ERC20Burnable {
+    /// @notice A constructor that mints the tokens
     constructor() ERC20("Bet45", "B45") {
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
